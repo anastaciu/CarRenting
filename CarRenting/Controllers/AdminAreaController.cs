ef6b63b166b5cafce7f4c90ed6f6810ae1fcd020
Ricardo <ricardojvsilva10@gmail.com>
2021-01-03 16:47:47 +0000

    checks and cartypes controllers

--- CarRenting/Controllers/AdminAreaController.cs
+++ CarRenting/Controllers/AdminAreaController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarRenting.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace CarRenting.Controllers
 {
@@ -11,7 +14,19 @@ namespace CarRenting.Controllers
         // GET: AdminArea
         public ActionResult Index()
         {
-            return View();
+            using (var db = new ApplicationDbContext())
+            {
+                var thisUser = db.Users.Find(User.Identity.GetUserId());
+                if (thisUser != null)
+                {
+                    var role = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().GetRoles(thisUser.Id).SingleOrDefault();
+
+                    var userViewModel = new UserDashViewModel()
+                        { ApplicationUser = thisUser, Role = role };
+                    return View(userViewModel);
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
